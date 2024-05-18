e0af8f75ff34e24d9e35e6c0824357dc363d907a
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-18 16:51:44 +0530

    addedckint

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,6 +7,7 @@ function About() {
       <h2>About Us</h2>
       <p>We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p>
       <h1> What Our clients talk abut us</h1>
+      <p>Really appriciate work of Developer services was good interaction and understanding the project requirements</p>
     </section>
   );
 }
