3017a6c07a0b67be59100bc39f95ff14551e51f6
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-18 16:49:26 +0530

    clint section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,6 +6,7 @@ function About() {
     <section id="about" className="about">
       <h2>About Us</h2>
       <p>We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p>
+      <h1> What Our clients talk abut us</h1>
     </section>
   );
 }
