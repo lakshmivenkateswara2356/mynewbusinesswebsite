9cc2cd271313b79bb94a10fe0522f66ec9cc1226
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-18 17:05:15 +0530

    addedone

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
     <section id="about" className="about">
       <h2>About Us</h2>
       <p>We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p>
-      <div>
+      <div className="clintsection">
       <h1> What Our clients talk abut us</h1>
       <p>Really appriciate work of Developer services was good interaction and understanding the project requirements</p>
       <p>One of the best services ever i ever seen </p>
