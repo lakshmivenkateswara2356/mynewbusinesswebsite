783ce3fad5708d013af7fcff7349fb544dfa4637
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-18 16:52:14 +0530

    div sect

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,8 +6,10 @@ function About() {
     <section id="about" className="about">
       <h2>About Us</h2>
       <p>We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p>
+      <div>
       <h1> What Our clients talk abut us</h1>
       <p>Really appriciate work of Developer services was good interaction and understanding the project requirements</p>
+      </div>
     </section>
   );
 }
