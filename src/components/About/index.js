12df68bbbbe44ae1045b7ceac144a3a2067cedf8
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-23 18:14:29 +0530

    commitclintupdate

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,12 +6,15 @@ function About() {
     <section id="about" className="about">
       <h2>About Us</h2>
       <p>We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p>
-      <div className="clintsection">
+      
       <h1> What Our clients talk abut us</h1>
-      <p>Really appriciate work of Developer services was good interaction and understanding the project requirements</p>
+      <div className="sizing">
+      <div className="clintser">
+      <p>Really appriciate work of Developer services was good interaction and understanding the project requirements</p></div>
+      </div>
       <p>One of the best services ever i ever seen </p>
       <h1>how ever</h1>
-      </div>
+     
     </section>
   );
 }
