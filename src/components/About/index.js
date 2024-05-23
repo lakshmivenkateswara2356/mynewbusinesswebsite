f8e5216827a297f8bd3f178e2ce081f8471deee8
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-23 18:36:16 +0530

    commitcorosel

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,48 @@
 import React from 'react';
-
+import norma from "../../Assets/norma.png"
 
 function About() {
   return (
-    <section id="about" className="about">
+    <div id="about" className="about">
       <h2>About Us</h2>
       <p>We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p>
       
       <h1> What Our clients talk abut us</h1>
       <div className="sizing">
+      
+      <div className="clintser">
+      <p>Really appriciate work of Developer services was good interaction and understanding the project requirements</p></div>
+
+      <div className="clintser">
+      <p>Really appriciate work of Developer services was good interaction and understanding the project requirements</p></div>
+
       <div className="clintser">
       <p>Really appriciate work of Developer services was good interaction and understanding the project requirements</p></div>
       </div>
       <p>One of the best services ever i ever seen </p>
       <h1>how ever</h1>
+
+
+
+      
+
+
      
-    </section>
+    </div>
+
+
+
+
+
+
   );
 }
 
 export default About;
+
+
+
+
+
+
+
