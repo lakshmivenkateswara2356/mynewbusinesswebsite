7529cff2246cc4186f707ed6e23fa4d801689854
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-05-28 10:48:23 +0530

    addedsymbol

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2024 developer services ♥️. All rights reserved.</p>
+      <p>&copy; 2024 developer services ❤️. All rights reserved.</p>
     </footer>
   );
 }
