49b6c3a24a17eb032b9a1e16716486227bebd6b5
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-02 20:54:34 +0530

    hero

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -11,6 +11,7 @@ function Services() {
           <h3>Frontend Development</h3>
           <p>Creating responsive and interactive user interfaces.</p>
           <p>using latest technologies was developing</p>
+          <h1>heeroo</h1>
         </div>
         <div className="service">
 
