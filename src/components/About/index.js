d93d398d55c389e2f264549d151f4e636fe06dfa
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-03 15:36:47 +0530

    hoot

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 function About() {
   return (
     <div id="about" className="about">
-     
       <h2>About Us</h2>
 <h1>we are providing quality services</h1>
       <p>We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p> 
