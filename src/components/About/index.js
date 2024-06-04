6454a4eaf49848ea5f0b82d2e05f12652b6a006f
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-04 11:01:51 +0530

    vvvv

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -19,6 +19,7 @@ function About() {
       <p>One of the best services ever i ever seen </p>
       <h1>we are providing quality services</h1>
       <h1>how ever</h1>
+      <p>they might be over</p>
     </div>
   );
 }
