5f25ba64442e69a331fd14a51ef9170db7f06cd8
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-04 12:16:24 +0530

    moreresponsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,31 @@
 import React from 'react';
 
+
 function Header({ toggleTheme, theme }) {
   return (
     <header className="header">
-      <h1 href="#about"> My Blog</h1>
+      <h1><a href="#about">My Blog</a></h1>
       <nav>
         <ul>
           <li><a href="#services">Services</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#contact">Contact</a></li>
-          <input
+          <li className="theme-toggle-container">
+            <input
               type="checkbox"
               id="theme-toggle"
               checked={theme === 'dark'}
               onChange={toggleTheme}
             />
-            <label htmlFor="theme-toggle">
-              {theme === 'light'}
+            <label htmlFor="theme-toggle" className="theme-toggle-label">
+              {theme === 'light' ? '☀️' : '🌙'}
             </label>
+          </li>
         </ul>
-        
-      </nav> 
+      </nav>
     </header>
   );
 }
 
 export default Header;
-
