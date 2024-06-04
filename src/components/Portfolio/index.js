a83d2492a325fee7338c99a3acf32d994c28f095
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-04 14:42:56 +0530

    someinitialcommits

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
 
+
 function Portfolio() {
   return (
     <section id="portfolio" className="portfolio">
-      <h2 className="portfolop">Portfolio</h2>
-      <div className="adjustmentlec"><h1 className="underline"></h1></div>
+      <h2 className="portfolio-title">Portfolio</h2>
+      <div className="underline"></div>
       <div className="portfolio-list">
         <div className="portfolio-item">
-          <h3>Chating app</h3>
-          <p>hfisgiy kseuf</p>
-          <p>Creating a real time chating application using react.js</p>
+          <h3>Chatting App</h3>
+          <p>Creating a real-time chatting application using React.js</p>
+          <button className="view-button">View Project</button>
         </div>
         <div className="portfolio-item">
           <h3>Nextrendz</h3>
-          <p>Shopping applicaton using technologies like html ,css, and javascript ,React.js</p>
+          <p>Shopping application using technologies like HTML, CSS, JavaScript, and React.js</p>
+          <button className="view-button">View Project</button>
         </div>
         <div className="portfolio-item">
           <h3>Dashboard</h3>
-          <p>Description of Project 3.</p>
-          <h1>mainf hedd</h1>
+          <p>Comprehensive dashboard for managing various data points efficiently.</p>
+          <button className="view-button">View Project</button>
         </div>
       </div>
     </section>
   );
 }
+
 export default Portfolio;
