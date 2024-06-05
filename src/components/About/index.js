e2869b17a59fdbad8555bf8c265efa2cf38a57b7
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-05 20:59:38 +0530

    univercommit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ function About() {
       <h2  className="aboutsection">About Us</h2>
       <div className="adjustmentlec"><h1 className="underline"></h1></div>
 <h1 className="abouthedingstyle">we are providing quality services</h1>
-<p>hettol jdjk jksk kks</p>
+
       <p className="allpara-styels">We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p> 
       
     </div>
