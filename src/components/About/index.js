6d8090f01e68cc9d5f3943408bc44c3f321cbe82
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-05 21:04:15 +0530

    commitenco

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,6 +7,7 @@ function About() {
       <h2  className="aboutsection">About Us</h2>
       <div className="adjustmentlec"><h1 className="underline"></h1></div>
       <h1>dualchanfes</h1>
+      <p1>fsjdh f kjdshfk khskdb  kjdks </p1>
 <h1 className="abouthedingstyle">we are providing quality services</h1>
       <p className="allpara-styels">We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p> 
       
