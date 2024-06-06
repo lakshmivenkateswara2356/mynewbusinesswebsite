b8a039192709869b1dad26903d4c5d64674730f0
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-06 15:25:12 +0530

    hoster

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,6 @@ function About() {
       <h2  className="aboutsection">About Us</h2>
       <div className="adjustmentlec"><h1 className="underline"></h1></div>
 <h1 className="abouthedingstyle">we are providing quality services</h1>
-
       <p className="allpara-styels">We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p> 
       
     </div>
