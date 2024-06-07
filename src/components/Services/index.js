86669dfb5f70b02a3ef3360cf02e9aa91727892c
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-07 12:59:24 +0530

    comitelement

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -6,7 +6,6 @@ function Services() {
     <section id="services" className="services">
       <h2 className="Oursection">Our Services</h2>
       <div className="adjustmentleccc"><h1 className="underline"></h1></div>
-      
       <div className="service-list">
         <div className="service">
           <h3>Frontend Development</h3>
