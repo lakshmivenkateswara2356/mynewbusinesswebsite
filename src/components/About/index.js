3eb6cea97f883264c055c3a55ab426652bda06c8
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-08 13:09:41 +0530

    sdfdf sdf

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,6 @@ function About() {
     <div id="about" className="about">
 
 
-
       <h2  className="aboutsection">About Us</h2>
       <div className="adjustmentlec"><h1 className="underline"></h1></div>
       <h1 className="abouthedingstyle">we are providing quality services</h1> 
