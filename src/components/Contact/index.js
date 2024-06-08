acea59bb5176e3036061187b1201cbfe04d53e64
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-08 12:38:22 +0530

    chagedcommits

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
+import { Linkedin, Mail,Github, CircleUserRound, } from 'lucide-react'
+
+
+
 
 
 function Contact() {
@@ -26,7 +30,7 @@ function Contact() {
           alert('Message sent successfully!');
       }, (error) => {
           console.log(error.text);
-          alert('Failed to send message. Please try again. or reach out me lakshmivenkateswarapunnam@gmail.com');
+          alert('Failed to send message. Please try again.');
       });
 
     setFormData({
@@ -42,32 +46,34 @@ function Contact() {
       <div className="adjustmentlec"><h1 className="underline"></h1></div>
       <div className="form-container">
         <form className="contact-form" onSubmit={handleSubmit}>
-          <label>
-            <input
-              type="text"
-              name="name"
-              className="input-field"
-              placeholder="Name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <label>
-            <input
-              type="email"
-              name="email"
-              className="input-field"
-              placeholder="Email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </label>
+          <div className="informaton-edt">
+            <label>
+              <input
+                type="text"
+                name="name"
+                className="input-field"
+                placeholder="Name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </label>
+            <label>
+              <input
+                type="email"
+                name="email"
+                className="input-field"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </label>
+          </div>
           <label>
             <textarea
               name="message"
-              className="textarea-field"
+              className="textarea-fieldee"
               placeholder="Enter your message here"
               value={formData.message}
               onChange={handleChange}
@@ -76,6 +82,28 @@ function Contact() {
           </label>
           <button type="submit" className="submit-button">Send</button>
         </form>
+      </div>
+      <div className='minisecpokl'>
+        <div className="koopw">
+      <div className='ContactVasuAlignee'>
+            <a href="mailto:lakshmivenkateswarapunnam@gmail.com" className='contactGmail1ee'>
+            <Mail />
+        </a>
+        </div>
+        <div className='ContactVasuAlignee'>
+       <a href='https://web.whatsapp.com+91 7842802368/' > <CircleUserRound className='contactGmail1ee'/> </a>
+       </div>
+       
+       <div className='ContactVasuAlignee'> 
+       <a href=" https://www.linkedin.com/in/p-lakshmi-venkateswara-144510236/"><Linkedin className='contactGmail1ee' /></a>
+       </div>
+       <div className='ContactVasuAlignee'>
+       <a href="https://github.com/lakshmivenkateswara2356"><Github className='contactGmail1ee' /></a>
+       </div>
+       </div>
+       
+       <h1>vasy</h1>
+       
       </div>
     </section>
   );
