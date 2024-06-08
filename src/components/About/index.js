2ac762faadd32c81079797fe85c8298f4235aa6f
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-08 13:10:50 +0530

    jhsag kj jkadhsjk

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,8 +8,6 @@ function About() {
       <div className="adjustmentlec"><h1 className="underline"></h1></div>
 
 
-
-      dfg
       <h1 className="abouthedingstyle">we are providing quality services</h1> 
       <p className="allpara-styels">We are a team of experienced fullstack and frontend developers dedicated to building high-quality web applications.</p> 
     </div>
