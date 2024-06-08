5faf350f3e6e5760dca76ec7785efcad09c9a8ad
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-08 13:10:00 +0530

    sjhd khjs dk

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,6 @@ function About() {
   return (
     <div id="about" className="about">
 
-
       <h2  className="aboutsection">About Us</h2>
       <div className="adjustmentlec"><h1 className="underline"></h1></div>
       <h1 className="abouthedingstyle">we are providing quality services</h1> 
