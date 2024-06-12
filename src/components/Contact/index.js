a4b05a96b4760d91824d81b353989573eb15be97
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-12 11:37:06 +0700

    jhg hjgjh

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -104,6 +104,7 @@ function Contact() {
           </div>
          <div className='looper'>
           <h1 className='herculous'>Contact</h1>
+          
           <p className='pooert'><a href='punnam8278@gmail.com'>punnam8278@gmail.com</a></p>
           <p>7842802368</p>
          </div>
